5010cfb91f804bd3f40d88849008fbf236a0a757
Dejan Stojanovic <dejan@evowition.com>
2022-06-15 22:00:16 +0400

    feat: initial commit

---
+++ Vault.Sample/Extensions/VaultConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace Vault.Sample.Extensions
+{
+    public class VaultConfiguration
+    {
+        public Uri Url { get; set; }
+        public string Backend { get; set; }
+        public string Secret { get; set; }
+        public string Token { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Vault.Sample/Extensions/VaultExtensions.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Flurl;
+
+namespace Vault.Sample.Extensions
+{
+    public static class VaultExtensions
+    {
+
+        public static IConfiguration AddVault(this IConfiguration configuration, string configurationSectionName)
+        {
+            return configuration.AddVault(configuration.GetSection(configurationSectionName));
+        }
+
+        public static IConfiguration AddVault(this IConfiguration configuration, IConfigurationSection configurationSection)
+        {
+            var vaultConfiguration = new VaultConfiguration();
+            configurationSection.Bind(vaultConfiguration);
+            return configuration.AddVault(vaultConfiguration);
+        }
+
+        public static IConfiguration AddVault(this IConfiguration configuration, Func<VaultConfiguration> vaultConfiguration)
+        {
+            return configuration.AddVault(vaultConfiguration.Invoke());
+        }
+
+        public static IConfiguration AddVault(this IConfiguration configuration, VaultConfiguration vaultConfiguration)
+        {
+            using (HttpClient secretHttpClient = new HttpClient())
+            {
+                string url = vaultConfiguration.Url.AppendPathSegment($"/v1/{vaultConfiguration.Backend}/data/{vaultConfiguration.Secret}");
+                if (!string.IsNullOrWhiteSpace(vaultConfiguration.Token))
+                {
+                    secretHttpClient.DefaultRequestHeaders.Add("X-Vault-Token", vaultConfiguration.Token);
+                }
+                else
+                {
+                    using (HttpClient authHttpClient = new HttpClient())
+                    {
+
+                    }
+                }
+                secretHttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = secretHttpClient.GetStringAsync(url).Result;
+
+                using var doc = JsonDocument.Parse(response);
+                var data = doc.RootElement.GetProperty("data").GetProperty("data");
+
+                var keys = data.EnumerateObject();
+                foreach (var key in keys)
+                {
+                    configuration[key.Name] = key.Value.GetString();
+                }
+
+                return configuration;
+            }
+        }
+
+    }
+}
